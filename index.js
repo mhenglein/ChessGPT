41e71b6c31417cc48086bd47e8df16cd668f0967
Marcus Henglein <marcus@henglein.com>
2023-02-22 10:49:17 +0100

    x

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ app.get("/ai-move", async (req, res) => {
   }
 
   // Calculate the AI's move (this is where you would call your AI algorithm)
-  const aiMove = await getNextMove(fen, an);
+  // const ascii = chess.ascii();
+  const prompt = await getNextMoveConsiderations(chess, an);
+  const aiMove = await getNextMove(prompt, fen);
 
   let move = chess.move(aiMove);
   if (move === null) {
@@ -44,22 +46,53 @@ app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
 
-async function getNextMove(fen, an, i = 0) {
+// COTA reasoning
+async function getNextMoveConsiderations(chess, an) {
   try {
-    const prompt = `You are Stockfish-Kasparov, a superintelligent chess computer that is a hybrid of the best chess engine and the best human chess player. What is your optimal move based on the FEN and AN below?
-    FEN: ${fen}
-    AN: ${an},`;
-    console.log(prompt);
+    const prompt = `You are Stockfish-Kasparov, a superintelligent chess computer that is a hybrid of the best chess engine and the best human chess player. You are playing a chess game defined by the FEN and AN below. You are playing as black.
+    FEN: ${chess.fen()}
+    AN: ${an}.
+    Are you (black) currently checked? ${chess.inCheck() ? "Yes" : "No"}
+    The chess board represented as ASCII looks like this:
+    ${chess.ascii()}
+    The following are a list of your available moves: ${chess.moves().join(", ")}
+    Let's think about this step by step. Explain the consequences of white's most recent move. What are your considerations for the top 3 optimal moves based on the current game so far?`;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0.7,
+      max_tokens: 400,
+      top_p: 1,
+      echo: true,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      n: 1,
+    });
+
+    return response.data.choices[0].text;
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+}
+
+async function getNextMove(prompt, fen, i = 0) {
+  try {
+    prompt = `${prompt}
+    Based on the considerations above, what could a next move look like? (Black to move)
+    MOVE (AN):`;
+
+    console.log("PROMPT", { prompt });
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt,
       temperature: 1,
       max_tokens: 4,
       top_p: 1,
-      stop: [",", "\n"],
+      stop: [",", "."],
       frequency_penalty: 0,
       presence_penalty: 0,
-      n: 10,
+      n: 20,
     });
 
     console.log(response.data.choices);
@@ -72,12 +105,27 @@ async function getNextMove(fen, an, i = 0) {
       possibleMoves.push(possibleMove);
     });
 
-    // Remove duplicates
-    possibleMoves = [...new Set(possibleMoves)];
-    console.log(possibleMoves);
-
     // Remove all single-letter moves
     possibleMoves = possibleMoves.filter((move) => move.length > 1);
+    console.log({ possibleMoves });
+
+    // Create a frequency map showing how many times each move appears
+    const frequencyMap = {};
+    possibleMoves.forEach((move) => {
+      if (frequencyMap[move]) {
+        frequencyMap[move]++;
+      } else {
+        frequencyMap[move] = 1;
+      }
+    });
+
+    // Sort the moves by frequency
+    possibleMoves = Object.keys(frequencyMap).sort((a, b) => frequencyMap[b] - frequencyMap[a]);
+    console.log({ possibleMoves });
+
+    // Remove duplicates
+    possibleMoves = [...new Set(possibleMoves)];
+    console.log("FINAL", { possibleMoves });
 
     // Pick the first valid move; if not, repeat the whole process; if not, pick a random move.
     const chess = new Chess(fen);
@@ -90,11 +138,14 @@ async function getNextMove(fen, an, i = 0) {
         console.log("Found a valid move", possibleMove);
         validMove = possibleMove;
         return validMove;
+      } else {
+        console.log("Invalid move", possibleMove);
       }
     }
 
+    // If over 3, return a random move
     if (i > 3) return moves[Math.floor(Math.random() * moves.length)];
-    return getNextMove(fen, an, i + 1);
+    return getNextMove(prompt, fen, i + 1);
   } catch (e) {
     console.log(e);
   }
